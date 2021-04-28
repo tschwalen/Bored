0cd8415f96fdb3218bc6c3fd698e95c980ddcf95
Thomas Schwalen <thomasschwalen@gmail.com>
2021-04-27 22:47:20 -0500

    added more parserstate code

--- cpp/parser.cpp
+++ cpp/parser.cpp
@@ -30,13 +30,47 @@ Token ParseState::matchKeyword(std::string &kwrd) {
     
     if ( tt == keyword && kwrd == tv )
         return advance();
-    else {
-        std::cout << "Expected keyword " << kwrd <<  " , encountered " 
-            << tokenTypeString(tt) << " with value " << tv << std::endl;
-        parsingError();
-        return Token { std::string(""), eof };
-    }
 
+    std::cout << "Expected keyword " << kwrd <<  " , encountered " 
+        << tokenTypeString(tt) << " with value " << tv << std::endl;
+    parsingError();
+    return Token { std::string(""), eof };
+}
+
+Token ParseState::matchTokenType(TokenType ttype) {
+    Token ct = currentToken();
+
+    if ( ct.type == ttype )
+        return advance();
+    
+    std::cout << "Expected token of type " << tokenTypeString(ttype) <<  " , encountered " 
+        << tokenTypeString(ct.type) << " with value " << ct.sval << std::endl;
+    parsingError();
+    return Token { std::string(""), eof };
+}
+
+Token ParseState::matchSymbol(std::string smbl) {
+    Token ct = currentToken();
+
+    if ( ct.type == symbol && ct.sval == smbl ) 
+        return advance();
+    
+    std::cout << "Expected symbol " << smbl <<  " , encountered " 
+        << tokenTypeString(ct.type) << " with value " << ct.sval << std::endl;
+    parsingError();
+    return Token { std::string(""), eof };
+}
+
+Token ParseState::matchLiteral() {
+    Token ct = currentToken();
+    TokenType tt = ct.type;
+    if ( tt == bool_literal || tt == int_literal || tt == real_literal || tt == string_literal )
+        return advance();
+
+    std::cout << "Expected literal value, encountered "  << tokenTypeString(ct.type) 
+        << " with value " << ct.sval << std::endl;
+    parsingError();
+    return Token { std::string(""), eof };
 }
 
 void ParseState::parsingError() {
